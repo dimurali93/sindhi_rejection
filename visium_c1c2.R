ec598881894901e0f37cb37896dfa2b7ace16f29
dimurali93 <dimurali@ucsd.edu>
2024-11-10 13:50:04 -0500

    DEGs and Markers for dsa versus no dsa in Rejectors

--- visium_c1c2.R
+++ visium_c1c2.R
@@ -1,23 +1,33 @@
-visiumc1 <- readRDS("~/Misc/sindhi/sindhiAll/FINALANALYSIS/Sindhi_Visium_FFPE_5R&2NR_C1/visiumc1.rds")
-visiumc2 <- readRDS("~/Misc/sindhi/sindhiAll/FINALANALYSIS/visiumc2/integrated_C2_subset_sct (1).RDS")
-DefaultAssay(visiumc1)="Spatial"
-DefaultAssay(visiumc2)="Spatial"
-visiumc1@assays$SCT<-NULL
-visiumc2@assays$SCT<-NULL
-visiumc1$batch ="visiumc1"
-visiumc2$batch ="visiumc2"
-library(Seurat)
-DimPlot(visiumc1)
-DimPlot(visiumc2)
-visiumc1.list <- SplitObject(visiumc1, split.by = "orig.ident")
-visiumc2.list <- SplitObject(visiumc2, split.by = "orig.ident")
-visiumall <- c(visiumc1.list, visiumc2.list)
-merged <- merge(visiumall[[1]],
-                visiumall[2:length(visiumall)])
+# visium_c1 <- readRDS("~/Misc/sindhi/sindhiAll/FINALANALYSIS/Sindhi_Visium_FFPE_5R&2NR_C1/visium_c1.rds")
+# visium_c2 <- readRDS("~/Misc/sindhi/sindhiAll/FINALANALYSIS/visium_c2/integrated_C2_subset_sct (1).RDS")
+visium_c1 <- readRDS("/ix1/rsindhi/dim95/Rprojects/sindhi_rejection/visium_c1/visiumc1.rds")
+
+visium_c2 <- readRDS("/ix1/rsindhi/dim95/Rprojects/sindhi_rejection/visium_c2/integrated_C2_subset_sct.RDS")
+
+
+DefaultAssay(visium_c1)="Spatial"
+DefaultAssay(visium_c2)="Spatial"
+
+visium_c1@assays$SCT<-NULL
+visium_c2@assays$SCT<-NULL
+
+visium_c1$batch ="visium_c1"
+visium_c2$batch ="visium_c2"
+
+DimPlot(visium_c1)
+DimPlot(visium_c2)
+
+visium_c1.list <- SplitObject(visium_c1, split.by = "orig.ident")
+visium_c2.list <- SplitObject(visium_c2, split.by = "orig.ident")
+
+visiumc1c2_rejection <- c(visium_c1.list, visium_c2.list)
+
+merged <- merge(visiumc1c2_rejection[[1]],
+                visiumc1c2_rejection[2:length(visiumc1c2_rejection)])
 merged <- RenameCells(merged,
                       new.names =paste("cell", 1:ncol(merged),
                                        sep = "_"))
-library(dplyr)
+
 merged=  NormalizeData(merged)
 merged =   FindVariableFeatures(merged,nfeatures = 3000)
 merged = ScaleData(merged)
@@ -31,15 +41,16 @@ markers <- FindAllMarkers(merged_rejector,
 markers <- dplyr::filter(markers,
                          p_val_adj < 0.01)
 
-visiumall <- visiumall[names(visiumall) %in% unique(merged_rejector$orig.ident)]
+
+visiumc1c2_rejection <- visiumc1c2_rejection[names(visiumc1c2_rejection) %in% unique(merged_rejector$orig.ident)]
 
 
-visiumall_2 <- lapply(X = visiumall, FUN = function(data) {
+visiumc1c2_rejection_2 <- lapply(X = visiumc1c2_rejection, FUN = function(data) {
   data <- data %>%
     NormalizeData() %>%
     FindVariableFeatures(nfeatures = 2000)
 })
-features <- SelectIntegrationFeatures(visiumall_2,
+features <- SelectIntegrationFeatures(visiumc1c2_rejection_2,
                                       nfeatures = 2000)
 batch_markers=markers
 batch_markers <-batch_markers %>%
@@ -48,27 +59,21 @@ batch_markers <-batch_markers %>%
 table(batch_markers$gene %in% features)
 features <- setdiff(features, batch_markers$gene)
 print(length(features))
-library(tidyverse)
-visiumall_3 <- lapply(X = visiumall_2, FUN = function(data) {
+
+visiumc1c2_rejection_3 <- lapply(X = visiumc1c2_rejection_2, FUN = function(data) {
   print(unique(data@meta.data$source))
   data <- data %>%
     ScaleData(features = features) %>%
     RunPCA(features = features)
 })
 # load selected integration features
-anchors <- FindIntegrationAnchors(object.list = visiumall_3,
+anchors <- FindIntegrationAnchors(object.list = visiumc1c2_rejection_3,
                                   normalization.method = "LogNormalize",
                                   anchor.features = features,
                                   reduction = "cca")
 integrated <- IntegrateData(anchorset = anchors,k.weight = 70)
 DefaultAssay(integrated) <- "integrated"
-# # run the standard workflow on the integrated assay
-# pct <- integrated[["pca"]]@stdev / sum(integrated[["pca"]]@stdev) * 100
-# cumu <- cumsum(pct)
-# co1 <- which(cumu > 90 & pct < 5)[1]
-# co2 <-sort(which((pct[1:length(pct) - 1] - pct[2:length(pct)]) > 0.1),
-#            decreasing = T)[1] + 1
-# ndims <- min(co1, co2)
+
 integrated <- integrated %>%
   ScaleData() %>%
   RunPCA(npcs = 10) %>%
@@ -81,8 +86,7 @@ DimPlot(integrated,split.by = "seurat_clusters")
 FeaturePlot(integrated, features = c("GLUL","ASS1"))
 SpatialDimPlot(integrated,crop = FALSE,ncol = 3)
 DefaultAssay(integrated)<-"Spatial"
-library(ggplot2)
-library(ggpubr)
+
 Idents(integrated)="seurat_clusters"
 Idents(integrated)="label"
 DotPlot(integrated, features =c( "GLUL", "ASS1","KRT7","SOX9","SCTR","AQP1","CFTR"))+
@@ -105,67 +109,60 @@ integrated$label <- Idents(integrated)
 DimPlot(integrated,label = T)
 Idents(integrated)="DSA"
 integrated = JoinLayers(integrated)
-integrated$dsa_label = paste0(integrated$DSA,"_",integrated$label)
 
 integrated$seu_label = paste0(integrated$seurat_clusters,"_",integrated$label)
 Idents(integrated)="seu_label"
-allmarkers =FindAllMarkers(integrated,logfc.threshold = .25)%>%filter(p_val<0.05)
-write.csv(allmarkers,"./Misc/sindhi/AllMarkers_Combinedvisiumc1c2_LFC.25_pval0.05.csv")
+
+markers_visiumc1c2_rejection_seu_label =FindAllMarkers(integrated,logfc.threshold = .25, only.pos = TRUE)%>%filter(p_val<0.05)
+write.csv(markers_visiumc1c2_rejection_seu_label,"./DEGList/Markers_Visiumc1c2_rejection_seu_label_LFC.25_pval0.05.csv")
 
 
 Idents(integrated)="label"
-allmarkers =FindAllMarkers(integrated,logfc.threshold = .25)%>%filter(p_val<0.05)
-write.csv(allmarkers,"./Misc/sindhi/AllMarkers_AnnotationCombinedvisiumc1c2_LFC.25_pval0.05.csv")
+markers_visiumc1c2_rejection_label =FindAllMarkers(integrated,logfc.threshold = .25, only.pos = TRUE)%>%filter(p_val<0.05)
+write.csv(markers_visiumc1c2_rejection_label,"./DEGList/Markers_Visiumc1c2_rejection_label_LFC.25_pval0.05.csv")
 
-
-
-
-
+integrated$dsa_label = paste0(integrated$DSA,"_",integrated$label)
 Idents(integrated)="dsa_label"
 DEG_Glul=FindMarkers(integrated,ident.1 = "yes_GLUL",ident.2 = "no_GLUL")%>%
   # filter(p_val_adj<0.05)%>%
   filter(p_val<0.05)%>%
-  filter(abs(avg_log2FC)>0.25)%>%rownames_to_column()%>%add_column(cluster ="GLUL_RDSAversusNoDSA")
+  filter(abs(avg_log2FC)>0.25)%>%rownames_to_column()%>%add_column(cluster ="visium_c1c2_rejector_glul_DSAvNoDSA")
 DEG_Cholangiocyte=FindMarkers(integrated,ident.1 = "yes_Cholangiocyte",ident.2 = "no_Cholangiocyte")%>%
   # filter(p_val_adj<0.05)%>%
   filter(p_val<0.05)%>%
-  filter(abs(avg_log2FC)>0.25)%>%rownames_to_column()%>%add_column(cluster ="cholangiocyte_RDSAversusNoDSA")
+  filter(abs(avg_log2FC)>0.25)%>%rownames_to_column()%>%add_column(cluster ="visium_c1c2_rejector_cholangiocyte_DSAvNoDSA")
 DEG_mid=FindMarkers(integrated,ident.1 = "yes_mid",ident.2 = "no_mid")%>%
   # filter(p_val_adj<0.05)%>%
   filter(p_val<0.05)%>%
-  filter(abs(avg_log2FC)>0.25)%>%rownames_to_column()%>%add_column(cluster ="mid_RDSAversusNoDSA")
+  filter(abs(avg_log2FC)>0.25)%>%rownames_to_column()%>%add_column(cluster ="visium_c1c2_rejector_mid_DSAvNoDSA")
 DEG_all=rbind(DEG_Glul,rbind(DEG_Cholangiocyte,DEG_mid))
 
+source("./GeneList.R")
+Restricted_DEGs_visium_c1c2_rejector_DSAvNoDSA <- DEG_all %>%
+  mutate(
+    TFH = ifelse(rowname %in% TFH, "TFH", NA),
+    GC = ifelse(rowname %in% GCMarker, "GC", NA),
+    HumoralImmunity = ifelse(rowname %in% humoralgenes, "HumoralImmunity", NA),
+    GC_TFs = ifelse(rowname %in% GC_TFs, "GC_TFs", NA),
+    TF_TFs = ifelse(rowname %in% TF_TFs, "TF_TFs", NA),
+    GCZones = ifelse(rowname %in% GCZones, "GCZones", NA),
+    cd40Signaling = ifelse(rowname %in% cd40Signaling, "cd40Signaling", NA),
+    nfkbactivation = ifelse(rowname %in% nfkbactivation, "nfkbactivation", NA),
+    ProliferationdarkZoneBcells = ifelse(rowname %in% ProliferationdarkZoneBcells, "ProliferationdarkZoneBcells", NA),
+    otherMarkers = ifelse(rowname %in% otherMarkers, "otherMarkers", NA),
+    calciumsignaling = ifelse(rowname %in% calciumsignaling, "calciumsignaling", NA),
+    BCRgenes = ifelse(rowname %in% BCRgenes, "BCRgenes", NA),
+    memoryBcells = ifelse(rowname %in% memoryBcells, "memoryBcells", NA)
+  )
 
-GCMarker = c( "CD38", "CD83","CXCR4","CXCR5","AICDA","IRF8","CD40","IGHG1")
-TFH = c("BCL6", "CXCR5", "ICOS", "IL21", "SH2D1A", "IRF4","PDCD1")
-library(dplyr)
-humoralgenes<- read_csv("./Misc/sindhi/GO_term_summary_20241031_140920.csv")
-humoralgenes=data.frame(humoralgenes)
-humoralgenes = humoralgenes[,"...12"]
 
+# RestrictedList <- RestrictedList %>%
+#   filter(!if_all(all_of(c("group3","group2","group")), is.na))
 
-humoralgenes=(unlist(humoralgenes))
-
-RestrictedList <- DEG_all %>%
-  mutate(group = case_when(
-    rowname %in% humoralgenes ~ "HumoralImmunity"))
-
-RestrictedList <- RestrictedList %>%
-  mutate(group2 = case_when(
-    rowname %in% TFH ~ "TFH"))
-
-RestrictedList <- RestrictedList %>%
-  mutate(group3 = case_when(
-    rowname %in% GCMarker ~ "GC"))
-
-RestrictedList <- RestrictedList %>%
-  filter(!if_all(all_of(c("group3","group2","group")), is.na))
-
-colnames(RestrictedList)=c("Gene","p_val","avg_log2FC","pct.1", "pct.2","p_val_adj","cluster", "HumoralImmunity","TFH","GC")
+# colnames(RestrictedList)=c("Gene","p_val","avg_log2FC","pct.1", "pct.2","p_val_adj","cluster", "HumoralImmunity","TFH","GC")
 
 # write.csv(RestrictedList,"./Misc/sindhi/DEG_all_visumc1c2_rdsaVsNoDSA_restrictedList.csv")
-write.csv(RestrictedList,"./Misc/sindhi/DEG_all_visumc1c2_rdsaVsNoDSA_restrictedList_LFC0.25_pval0.05.csv")
+write.csv(Restricted_DEGs_visium_c1c2_rejector_DSAvNoDSA,"./DEGList/DEGs_visium_c1c2_rejector_DSAvNoDSA_LFC.25_pval.05.csv")
 
 # write.csv(DEG_all,"./Misc/sindhi/DEG_all_visumc1c2_rdsaVsNoDSA.csv")
 write.csv(DEG_all,"./Misc/sindhi/DEG_all_visumc1c2_rdsaVsNoDSA_LFC0.25_pval0.05.csv")
@@ -179,99 +176,3 @@ DotPlot(integrated, features =c( "CD38", "CD83","CXCR4","CXCR5","AICDA","IRF8","
   scale_colour_gradient2(low = "blue", mid = "white", high = "red")
 
 DoHeatmap(integrated, features = c("CD38", "CD83","CXCR4","CXCR5","AICDA","IRF8","CD40","IGHG1"),assay = "integrated")
-
-GCMarker = c( "CD38", "CD83","CXCR4","CXCR5","AICDA","IRF8","CD40","IGHG1")
-TFH = c("BCL6", "CXCR5", "ICOS", "IL21", "SH2D1A", "IRF4","PDCD1")
-library(dplyr)
-humoralgenes<- read_csv("Misc/sindhi/GO_term_summary_20241031_140920.csv")
-humoralgenes=data.frame(humoralgenes)
-humoralgenes = humoralgenes%>%
-  # filter(Annotated.Term =="humoral immune response mediated by circulating immunoglobulin")%>%
-  select("...12")
-
-
-
-Obj <- ScaleData(integrated, features = rownames(integrated))
-
-
-
-
-DotPlot(integrated, features =unique(humoralgenes))+
-  scale_colour_gradient2(low = "blue", mid = "white", high = "red")
-DoHeatmap(Obj, features = unlist(unique(humoralgenes)))
-DoHeatmap(Obj, features = GCMarker)
-DoHeatmap(Obj, features = TFH)
-
-
-
-
-
-##################################
-DEG_all_visumc1c2_rdsaVsNoDSA_LFC0_25_pval0_05 <- read.csv("Misc/sindhi/DEG_all_visumc1c2_rdsaVsNoDSA_LFC0.25_pval0.05.csv")
-DEG_all = DEG_all_visumc1c2_rdsaVsNoDSA_LFC0_25_pval0_05
-GCMarker = c( "CD38", "CD83","CXCR4","CXCR5","AICDA","IRF8","CD40","IGHG1")
-TFH = c("BCL6", "CXCR5", "ICOS", "IL21", "SH2D1A", "IRF4","PDCD1")
-library(dplyr)
-humoralgenes<- read_csv("./Misc/sindhi/GO_term_summary_20241031_140920.csv")
-humoralgenes=data.frame(humoralgenes)
-humoralgenes = humoralgenes[,"...12"]
-
-
-humoralgenes=(unlist(humoralgenes))
-
-TF_TFs = as.set(c("CD4", "IL21", "PDCD1", "CXCR3", "CCR6", "ICOS", "IRF4", "BCL6", "CXCR5", "CXCL13", "IL4","ASCL2", "TCF", "MAF", "BATF"))
-GC_TFs = as.set(c("BCL6", "FOXO1", "FOXP1" ))
-GCZones= as.set(c("CD83" , "CXCR4"))
-cd40Signaling = as.set(c("CD40", "TRAF1", "ICAM1"))
-nfkbactivation = as.set(c("NFKB2", "RELB", "NFKB1", "REL"))
-ProliferationdarkZoneBcells =as.set(c("PCNA", "MKI67", "CDK1", "CDC20", "MYC"))
-memoryBcells = as.set(c("PRDM1", "CCR6", "CD27", "XBP1", "IRF4", "MZB1", "TNFRSF17"))
-otherMarkers = as.set(c("AICDA" , "BCL2A1", "EZH2", "E2F1"))
-calciumsignaling =as.set(c("PTPN6", "CD22"))
-BCRgenes=as.set(c("BTK", "BLK", "BLNK"))
-
-
-RestrictedListvisiumc1c2 <-mutate(DEG_all,TFH = case_when(
-  rowname %in% TFH ~ "TFH"))
-
-RestrictedListvisiumc1c2 <-  mutate(RestrictedListvisiumc1c2,GC = case_when(
-  rowname %in% GCMarker ~ "GC"))
-
-RestrictedListvisiumc1c2 <-  mutate(RestrictedListvisiumc1c2,HumoralImmunity = case_when(
-  rowname %in% humoralgenes ~ "HumoralImmunity"))
-
-RestrictedListvisiumc1c2 <-  mutate(RestrictedListvisiumc1c2,GC_TFs = case_when(
-  rowname %in% GC_TFs ~ "GC_TFs"))
-
-RestrictedListvisiumc1c2 <-  mutate(RestrictedListvisiumc1c2,TF_TFs = case_when(
-  rowname %in% TF_TFs ~ "TF_TFs"))
-
-RestrictedListvisiumc1c2 <-  mutate(RestrictedListvisiumc1c2,GCZones = case_when(
-  rowname %in% GCZones ~ "GCZones"))
-
-RestrictedListvisiumc1c2 <-  mutate(RestrictedListvisiumc1c2,cd40Signaling = case_when(
-  rowname %in% cd40Signaling ~ "cd40Signaling"))
-
-RestrictedListvisiumc1c2 <-  mutate(RestrictedListvisiumc1c2,nfkbactivation = case_when(
-  rowname %in% nfkbactivation ~ "nfkbactivation"))
-
-RestrictedListvisiumc1c2 <-  mutate(RestrictedListvisiumc1c2,ProliferationdarkZoneBcells = case_when(
-  rowname %in% ProliferationdarkZoneBcells ~ "ProliferationdarkZoneBcells"))
-
-
-RestrictedListvisiumc1c2 <-  mutate(RestrictedListvisiumc1c2,otherMarkers= case_when(
-  rowname %in% otherMarkers ~ "otherMarkers"))
-
-RestrictedListvisiumc1c2 <-  mutate(RestrictedListvisiumc1c2,calciumsignaling= case_when(
-  rowname %in% calciumsignaling ~ "calciumsignaling"))
-
-RestrictedListvisiumc1c2 <-  mutate(RestrictedListvisiumc1c2,BCRgenes= case_when(
-  rowname %in% BCRgenes ~ "BCRgenes"))
-
-RestrictedListvisiumc1c2 <-  mutate(RestrictedListvisiumc1c2,memoryBcells = case_when(
-  rowname %in% memoryBcells ~ "memoryBcells"))
-
-
-RestrictedListvisiumc1c2 <-   filter(RestrictedListvisiumc1c2,!if_all(c("memoryBcells","BCRgenes","calciumsignaling","otherMarkers","ProliferationdarkZoneBcells","cd40Signaling","GCZones","TF_TFs","GC_TFs","HumoralImmunity","GC","TFH","nfkbactivation"), is.na))
-
-write.csv(RestrictedListvisiumc1c2,"~/Misc/sindhi/DEGs_RestrictedListvisiumc1c2_expanded.csv")
--- visium_c2/visium_c2.R
+++ visium_c2/visium_c2.R
@@ -1,86 +1,111 @@
-integrated_C2_subset_sct <- readRDS("~/Misc/sindhi/sindhiAll/FINALANALYSIS/visiumc2/integrated_C2_subset_sct (1).RDS")
+# integrated_C2_subset_sct <- readRDS("~/Misc/sindhi/sindhiAll/FINALANALYSIS/visiumc2/integrated_C2_subset_sct (1).RDS")
+source("./1.library.R")
 
-Idents(integrated_C2_subset_sct)="Outcome"
-integrated_C2_subset_sct_rejector = subset(integrated_C2_subset_sct, idents = c( "Rejector"))
-remove(integrated_C2_subset_sct)
+visium_c2 <- readRDS("./visium_c2/integrated_C2_subset_sct.RDS")
 
-integrated_C2_subset_sct_rejector <- SCTransform(integrated_C2_subset_sct_rejector, vars.to.regress = "percent.mt", verbose = FALSE, assay = "Spatial")%>%
+Idents(visium_c2)="label_scRNA_integ"
+visiumc2_marker = FindAllMarkers(visium_c2,logfc.threshold = .25,only.pos = TRUE )%>%dplyr::filter(p_val<0.05)
+write.csv(visiumc2_marker,"./DEGList/Markers_visium_c2_LFC0.25_pval0.05.csv")
+
+Idents(visium_c2)="Outcome"
+visium_c2_rejector = subset(visium_c2, idents = c( "Rejector"))
+
+visium_c2_rejector <- SCTransform(visium_c2_rejector,
+                                  vars.to.regress = "percent.mt", verbose = FALSE, assay = "Spatial")%>%
   RunPCA(npcs = 30, verbose = FALSE) %>%
   RunUMAP(reduction = "pca", dims = 1:10, verbose = FALSE) %>%
-  FindNeighbors(reduction = "pca", dims = 1:10, verbose = FALSE) %>%
-  FindClusters(resolution = 0.5, verbose = FALSE)
+  FindNeighbors(reduction = "pca", dims = 1:10, verbose = FALSE)
+# %>% FindClusters(resolution = 0.5, verbose = FALSE)
 
-integrated_C2_subset_sct_rejector = PrepSCTFindMarkers(integrated_C2_subset_sct_rejector)
-Idents(integrated_C2_subset_sct_rejector) = "label_scRNA_integ"
+visium_c2_rejector = PrepSCTFindMarkers(visium_c2_rejector)
+Idents(visium_c2_rejector) = "label_scRNA_integ"
 
-visiumc2_marker = FindAllMarkers(integrated_C2_subset_sct_rejector,logfc.threshold = .25 )%>%dplyr::filter(p_val<0.05)
+visiumc2_rejector_marker = FindAllMarkers(visium_c2_rejector,logfc.threshold = .25,only.pos = TRUE )%>%dplyr::filter(p_val<0.05)
 
-integrated_C2_subset_sct_rejector$anno_dsa = paste0(integrated_C2_subset_sct_rejector$label_scRNA_integ,"_",integrated_C2_subset_sct_rejector$DSA)
+write.csv(visiumc2_rejector_marker,"./DEGList/Markers_visium_c2_rejection_LFC0.25_pval0.05.csv")
 
-Idents(integrated_C2_subset_sct_rejector)="anno_dsa"
-
-DEGs_Visiumc2_RejectionGlul = FindMarkers(integrated_C2_subset_sct_rejector, ident.1 ="GLUL_yes", ident.2 = "GLUL_no" )%>%
+visium_c2_rejector$anno_dsa = paste0(visium_c2_rejector$label_scRNA_integ,"_",visium_c2_rejector$DSA)
+Idents(visium_c2_rejector)="anno_dsa"
+DEGs_visium_c2_rejector_Glul_DSAvNoDSA = FindMarkers(visium_c2_rejector, ident.1 ="GLUL_yes", ident.2 = "GLUL_no" )%>%
   dplyr::filter(abs(avg_log2FC)>0.25)%>%
   filter(p_val<0.05)%>%rownames_to_column()%>%
-  mutate(condition = "Glul_RejectorDSA")
+  mutate(condition = "visium_c2_rejector_Glul_DSAvNoDSA")
 
-DEGs_Visiumc2_RejectionMidzone = FindMarkers(integrated_C2_subset_sct_rejector, ident.1 ="Midzone_yes", ident.2 = "Midzone_no" )%>%
+DEGs_visium_c2_rejector_Midzone_DSAvNoDSA = FindMarkers(visium_c2_rejector, ident.1 ="Midzone_yes", ident.2 = "Midzone_no" )%>%
   # dplyr::filter(abs(avg_log2FC)>0.25)%>%
   filter(p_val<0.05)%>%rownames_to_column()%>%
-  mutate(condition = "Midzone_RejectorDSA")
+  mutate(condition = "visium_c2_rejector_Midzone_DSAvNoDSA")
 
-DEGs_Visiumc2_RejectionCholangiocytes = FindMarkers(integrated_C2_subset_sct_rejector, ident.1 ="Cholangiocytes_yes", ident.2 = "Cholangiocytes_no" )%>%
+DEGs_visium_c2_rejector_Cholangiocytes_DSAvNoDSA = FindMarkers(visium_c2_rejector, ident.1 ="Cholangiocytes_yes", ident.2 = "Cholangiocytes_no" )%>%
   dplyr::filter(abs(avg_log2FC)>0.25)%>%
   filter(p_val<0.05)%>%rownames_to_column()%>%
-  mutate(condition = "Cholangiocytes_RejectorDSA")
+  mutate(condition = "visium_c2_rejector_Cholangiocytes_DSAvNoDSA")
 
 
-DEGs_Visiumc2_Rejection =  rbind(DEGs_Visiumc2_RejectionGlul,rbind(DEGs_Visiumc2_RejectionMidzone,DEGs_Visiumc2_RejectionCholangiocytes))
+DEGs_visium_c2_rejector_DSAvNoDSA =  rbind(DEGs_visium_c2_rejector_Glul_DSAvNoDSA,rbind(DEGs_visium_c2_rejector_Midzone_DSAvNoDSA,DEGs_visium_c2_rejector_Cholangiocytes_DSAvNoDSA))
+
+source("./GeneList.R")
+# Restricted_DEGs_visium_c2_rejector_DSAvNoDSA <-mutate(DEGs_visium_c2_rejector_DSAvNoDSA,TFH = case_when(
+#   rowname %in% TFH ~ "TFH"))
+#
+# Restricted_DEGs_visium_c2_rejector_DSAvNoDSA <-  mutate(Restricted_DEGs_visium_c2_rejector_DSAvNoDSA,GC = case_when(
+#   rowname %in% GCMarker ~ "GC"))
+#
+# Restricted_DEGs_visium_c2_rejector_DSAvNoDSA <-  mutate(Restricted_DEGs_visium_c2_rejector_DSAvNoDSA,HumoralImmunity = case_when(
+#   rowname %in% humoralgenes ~ "HumoralImmunity"))
+#
+# Restricted_DEGs_visium_c2_rejector_DSAvNoDSA <-  mutate(Restricted_DEGs_visium_c2_rejector_DSAvNoDSA,GC_TFs = case_when(
+#   rowname %in% GC_TFs ~ "GC_TFs"))
+#
+# Restricted_DEGs_visium_c2_rejector_DSAvNoDSA <-  mutate(Restricted_DEGs_visium_c2_rejector_DSAvNoDSA,TF_TFs = case_when(
+#   rowname %in% TF_TFs ~ "TF_TFs"))
+#
+# Restricted_DEGs_visium_c2_rejector_DSAvNoDSA <-  mutate(Restricted_DEGs_visium_c2_rejector_DSAvNoDSA,GCZones = case_when(
+#   rowname %in% GCZones ~ "GCZones"))
+#
+# Restricted_DEGs_visium_c2_rejector_DSAvNoDSA <-  mutate(Restricted_DEGs_visium_c2_rejector_DSAvNoDSA,cd40Signaling = case_when(
+#   rowname %in% cd40Signaling ~ "cd40Signaling"))
+#
+# Restricted_DEGs_visium_c2_rejector_DSAvNoDSA <-  mutate(Restricted_DEGs_visium_c2_rejector_DSAvNoDSA,nfkbactivation = case_when(
+#   rowname %in% nfkbactivation ~ "nfkbactivation"))
+#
+# Restricted_DEGs_visium_c2_rejector_DSAvNoDSA <-  mutate(Restricted_DEGs_visium_c2_rejector_DSAvNoDSA,ProliferationdarkZoneBcells = case_when(
+#   rowname %in% ProliferationdarkZoneBcells ~ "ProliferationdarkZoneBcells"))
+#
+# Restricted_DEGs_visium_c2_rejector_DSAvNoDSA <-  mutate(Restricted_DEGs_visium_c2_rejector_DSAvNoDSA,otherMarkers= case_when(
+#   rowname %in% otherMarkers ~ "otherMarkers"))
+#
+# Restricted_DEGs_visium_c2_rejector_DSAvNoDSA <-  mutate(Restricted_DEGs_visium_c2_rejector_DSAvNoDSA,calciumsignaling= case_when(
+#   rowname %in% calciumsignaling ~ "calciumsignaling"))
+#
+# Restricted_DEGs_visium_c2_rejector_DSAvNoDSA <-  mutate(Restricted_DEGs_visium_c2_rejector_DSAvNoDSA,BCRgenes= case_when(
+#   rowname %in% BCRgenes ~ "BCRgenes"))
+#
+# Restricted_DEGs_visium_c2_rejector_DSAvNoDSA <-  mutate(Restricted_DEGs_visium_c2_rejector_DSAvNoDSA,memoryBcells = case_when(
+#   rowname %in% memoryBcells ~ "memoryBcells"))
 
 
-Restricted_DEGs_Visiumc2_Rejection <-mutate(DEGs_Visiumc2_Rejection,TFH = case_when(
-  rowname %in% TFH ~ "TFH"))
+Restricted_DEGs_visium_c2_rejector_DSAvNoDSA <- DEGs_visium_c2_rejector_DSAvNoDSA %>%
+  mutate(
+    TFH = ifelse(rowname %in% TFH, "TFH", NA),
+    GC = ifelse(rowname %in% GCMarker, "GC", NA),
+    HumoralImmunity = ifelse(rowname %in% humoralgenes, "HumoralImmunity", NA),
+    GC_TFs = ifelse(rowname %in% GC_TFs, "GC_TFs", NA),
+    TF_TFs = ifelse(rowname %in% TF_TFs, "TF_TFs", NA),
+    GCZones = ifelse(rowname %in% GCZones, "GCZones", NA),
+    cd40Signaling = ifelse(rowname %in% cd40Signaling, "cd40Signaling", NA),
+    nfkbactivation = ifelse(rowname %in% nfkbactivation, "nfkbactivation", NA),
+    ProliferationdarkZoneBcells = ifelse(rowname %in% ProliferationdarkZoneBcells, "ProliferationdarkZoneBcells", NA),
+    otherMarkers = ifelse(rowname %in% otherMarkers, "otherMarkers", NA),
+    calciumsignaling = ifelse(rowname %in% calciumsignaling, "calciumsignaling", NA),
+    BCRgenes = ifelse(rowname %in% BCRgenes, "BCRgenes", NA),
+    memoryBcells = ifelse(rowname %in% memoryBcells, "memoryBcells", NA)
+  )
 
-Restricted_DEGs_Visiumc2_Rejection <-  mutate(Restricted_DEGs_Visiumc2_Rejection,GC = case_when(
-  rowname %in% GCMarker ~ "GC"))
+# Restricted_DEGs_visium_c2_rejector_DSAvNoDSA <-   filter(Restricted_DEGs_visium_c2_rejector_DSAvNoDSA,!if_all(c("memoryBcells","BCRgenes","calciumsignaling","otherMarkers","ProliferationdarkZoneBcells","cd40Signaling","GCZones","TF_TFs","GC_TFs","HumoralImmunity","GC","TFH","nfkbactivation"), is.na))
 
-Restricted_DEGs_Visiumc2_Rejection <-  mutate(Restricted_DEGs_Visiumc2_Rejection,HumoralImmunity = case_when(
-  rowname %in% humoralgenes ~ "HumoralImmunity"))
+write.csv(Restricted_DEGs_visium_c2_rejector_DSAvNoDSA,"./DEGList/Restricted_DEGs_visium_c2_rejector_DSAvNoDSA.csv")
 
-Restricted_DEGs_Visiumc2_Rejection <-  mutate(Restricted_DEGs_Visiumc2_Rejection,GC_TFs = case_when(
-  rowname %in% GC_TFs ~ "GC_TFs"))
-
-Restricted_DEGs_Visiumc2_Rejection <-  mutate(Restricted_DEGs_Visiumc2_Rejection,TF_TFs = case_when(
-  rowname %in% TF_TFs ~ "TF_TFs"))
-
-Restricted_DEGs_Visiumc2_Rejection <-  mutate(Restricted_DEGs_Visiumc2_Rejection,GCZones = case_when(
-  rowname %in% GCZones ~ "GCZones"))
-
-Restricted_DEGs_Visiumc2_Rejection <-  mutate(Restricted_DEGs_Visiumc2_Rejection,cd40Signaling = case_when(
-  rowname %in% cd40Signaling ~ "cd40Signaling"))
-
-Restricted_DEGs_Visiumc2_Rejection <-  mutate(Restricted_DEGs_Visiumc2_Rejection,nfkbactivation = case_when(
-  rowname %in% nfkbactivation ~ "nfkbactivation"))
-
-Restricted_DEGs_Visiumc2_Rejection <-  mutate(Restricted_DEGs_Visiumc2_Rejection,ProliferationdarkZoneBcells = case_when(
-  rowname %in% ProliferationdarkZoneBcells ~ "ProliferationdarkZoneBcells"))
-
-Restricted_DEGs_Visiumc2_Rejection <-  mutate(Restricted_DEGs_Visiumc2_Rejection,otherMarkers= case_when(
-  rowname %in% otherMarkers ~ "otherMarkers"))
-
-Restricted_DEGs_Visiumc2_Rejection <-  mutate(Restricted_DEGs_Visiumc2_Rejection,calciumsignaling= case_when(
-  rowname %in% calciumsignaling ~ "calciumsignaling"))
-
-Restricted_DEGs_Visiumc2_Rejection <-  mutate(Restricted_DEGs_Visiumc2_Rejection,BCRgenes= case_when(
-  rowname %in% BCRgenes ~ "BCRgenes"))
-
-Restricted_DEGs_Visiumc2_Rejection <-  mutate(Restricted_DEGs_Visiumc2_Rejection,memoryBcells = case_when(
-  rowname %in% memoryBcells ~ "memoryBcells"))
-
-
-Restricted_DEGs_Visiumc2_Rejection <-   filter(Restricted_DEGs_Visiumc2_Rejection,!if_all(c("memoryBcells","BCRgenes","calciumsignaling","otherMarkers","ProliferationdarkZoneBcells","cd40Signaling","GCZones","TF_TFs","GC_TFs","HumoralImmunity","GC","TFH","nfkbactivation"), is.na))
-
-write.csv(RestrictedListvisiumc2,"/home/divya/Misc/sindhi/DEGs_RestrictedListvisiumc2_expanded.csv")
 
 
 
